6920c2de1123a909250de188aaa75f0fdc63a1c0
Leon Simmonds <lsimmonds@reachlocal.com>
2013-08-05 14:43:39 -0700

    more test files

---
+++ rl_config.js
@@ -0,0 +1,52 @@
+window.rl_widget_cfg = {
+  "id": "b4c3a40e-a7d8-45e0-b8bd-3835449492d6",
+  "globalMasterAdvertiserId": "USA_8967",
+  "config": {
+    "domains": {
+      "cdn": "static.rlcdn.net",
+      "mms": "rtsys.rlmms.com",
+      "capture": "rlets.com",
+      "internal": [
+        "http://lsimmonds.github.io",
+        "lvh.me"
+      ]
+    },
+    "platform": "USA",
+    "debug": 0,
+    "hipaa": 0,
+    "optOut": null
+  },
+  "products": [
+    {
+      "name": "capture",
+      "enabled": true,
+      "autoload": true,
+      "jsFile": "/capture/rl_capture.source.js",
+      "jsCode": "",
+      "cssFile": "",
+      "config": {
+      }
+    }
+  ],
+  "replacements": {
+    "DIRECT": {
+      "strings": [],
+      "phone": [],
+      "email": []
+    }
+  },
+  "cvts": {
+  },
+  "campaign_data": {
+    "DIRECT": {
+      "referrer_type": "ORGANIC",
+      "master_campaign_id": "1226468",
+      "campaign_name": "Apple Annies Restaurant 20130121"
+    }
+  },
+  "blacklist": {
+    "enabled": true,
+    "items": {
+    }
+  }
+};
